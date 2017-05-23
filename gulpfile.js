509c8a05acb28287012eb5e8492a47d9458b3ba1
pmutua <pmutua@live.com>
2017-05-23 12:49:26 +0300

    add gulp file run task

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var lib = require('bower-files')();
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
